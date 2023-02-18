92468470c7e73bc9b1f246b60fc29d2f903f0228
amir <amirnobari1990@gmail.com>
2023-02-18 21:49:40 +0330

    Add login and register validator

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -2,7 +2,20 @@ const validator = require("./validator");
 const { check } = require("express-validator");
 
 module.exports = new class UserValidator extends validator {
-  handle() {
+  register() {
+    return [
+       // username must be an name
+       check("name", "نام کاربری وارد نشده است").not().isEmpty(),
+      // username must be an email
+      check("email", "فرمت ایمیل صحیح نیست").isEmail(),
+      // password must be at least 5 chars long
+      check("password", "طول پسورد بایستی بیشتر از 5 کاراکتر باشد").isLength({
+        min: 5,
+      }),
+    ];
+  }
+
+  login() {
     return [
       // username must be an email
       check("email", "فرمت ایمیل صحیح نیست").isEmail(),
@@ -12,4 +25,5 @@ module.exports = new class UserValidator extends validator {
       }),
     ];
   }
+
 };
