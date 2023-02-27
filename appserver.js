9627cde4a53670493ee38d2fb7750954e3f1fbe3
amir <amirnobari1990@gmail.com>
2023-02-27 20:37:03 +0330

    added a new config package(passport,bcryptjs)

--- appserver.js
+++ appserver.js
@@ -5,10 +5,10 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const flash = require("connect-flash");
 const mongoose = require("mongoose");
+const passport=require('passport');
 const MongoStore = require("connect-mongo");
 
 require("app-module-path").addPath(__dirname);
-
 require("dotenv").config();
 
 mongoose
@@ -16,13 +16,13 @@ mongoose
   .then(() => console.log("Connected to server!"));
 
 global.config = require("./config");
-
+app.use(flash());
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(methodOverride("method"));
-app.use(flash());
 app.use(cookieParser(process.env.cookie_secret));
+
 //console.log(process.env.session_secret);
 app.use(
   session({
@@ -31,15 +31,19 @@ app.use(
     saveUninitialized: true,
     cookie: { expires: new Date(Date.now() + 1000 * 3600 * 24 * 7) },
     store: MongoStore.create({
-      mongoUrl: "mongodb://127.0.0.1:27017/firstnode",
-    }),
+      mongoUrl: "mongodb://127.0.0.1:27017/firstnode"}),
   })
 );
 
+require('./passport/passport-local');
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use((req, res, next) => {
-  res.locals = { errors: req.flash("errors") };
+  res.locals = { errors: req.flash("errors"),req};
   next();
 });
+
 app.use("/", require("./routes/index"));
 
 app.listen(config.port, () => {
