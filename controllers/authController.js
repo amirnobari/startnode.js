657d434571d0320e0d2d11a6ee1d16861c77c0e0
amir <amirnobari1990@gmail.com>
2023-03-03 19:06:24 +0330

    added recaptcha

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,16 @@
 let controller = require("./controller");
 const User = require("models/user");
 const passport = require("passport");
+const Recaptcha = require('express-recaptcha').RecaptchaV2
+const options = { hl: 'fa' };
+const recaptcha = new Recaptcha('6LfG2swkAAAAAAL7Q8nJjllFJEPCodRCGYQOJAcW', '6LfG2swkAAAAAEWhmF50JBzEz01isFOk6rwi-ZjE', options);
 const { validationResult } = require("express-validator");
+const { Promise } = require("mongoose");
 
 class UserController extends controller {
   async registerform(req, res, next) {
     try {
-      res.render("auth/register");
+      res.render("auth/register",{recaptcha:recaptcha.render()});
     } catch (err) {
       next(err);
     }
@@ -14,7 +18,7 @@ class UserController extends controller {
 
   async loginform(req, res, next) {
     try {
-      res.render("auth/login");
+      res.render("auth/login",{recaptcha:recaptcha.render()});
     } catch (err) {
       next(err);
     }
@@ -22,6 +26,22 @@ class UserController extends controller {
 
   async login(req, res, next) {
     try {
+      let recaptchaResult = await new Promise((resolve,reject)=>{
+        recaptcha.verify(req,(err,data)=>{
+          if(err){
+            req.flash('errors','تیک امنیتی را بزنید');
+            res.redirect('/auth/login');
+            resolve(false)
+          }else{
+            resolve(true)
+          }
+        })
+       })
+  if(!recaptchaResult){
+    return;
+  }
+
+
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         req.flash("errors", errors.array());
@@ -41,6 +61,21 @@ class UserController extends controller {
 
   async register(req, res, next) {
     try {
+     let recaptchaResult = await new Promise((resolve,reject)=>{
+      recaptcha.verify(req,(err,data)=>{
+        if(err){
+          req.flash('errors','تیک امنیتی را بزنید');
+          res.redirect('/auth/register');
+          resolve(false)
+        }else{
+          resolve(true)
+        }
+      })
+     })
+if(!recaptchaResult){
+  return;
+}
+
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         req.flash("errors", errors.array());
